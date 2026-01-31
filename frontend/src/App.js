537da7e75ff1bc04deacfe3e555b2ecf42202be5
My Name <varpe@gmail.com>
2026-01-31 22:32:47 +0530

    Backend auth: JWT + owner role

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-8">
+      <h1 className="text-4xl font-bold text-blue-600">Cafe POS Frontend Ready!</h1>
+    </div>
+  );
+}
+
+export default App;
